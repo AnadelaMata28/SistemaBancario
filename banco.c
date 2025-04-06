2c3a7ea6bf1d3d71245535b1c23c0fe9e56505d2
AnadelaMata28 <ana.delamatadelai@gmail.com>
2025-04-06 17:53:21 +0200

    errores

--- banco.c
+++ banco.c
@@ -9,6 +9,8 @@
 #include <sys/wait.h>
 #include <pthread.h>
 #include <time.h>
+#include <semaphore.h>
+#include <fcntl.h>
 #include "init_cuentas.h"
 #include "estructura.h"
 
@@ -23,20 +25,22 @@ typedef struct Config {
    char archivo_log[50];
 } Config;
 
-int semid; //Semáforo para controlar el acceso a las cuentas de los usuarios
+sem_t *sem_banco; //Semáforo para controlar el acceso a las cuentas de los usuarios
 int pipe_padre_hijo[2]; //Tubería para la comunicación de los procesos padres con los procesos hijos
 
 Config leer_configuracion(const char *ruta) {
+	sem_wait(sem_banco);
 	FILE *archivo = fopen(ruta, "r");
 
 	if (archivo == NULL) {
         	perror("Error al abrir config.txt");
+			sem_post(sem_banco);
 	        exit(1);
    	}
 
-        Config config;
-        char linea[100];
-        while (fgets(linea, sizeof(linea), archivo)) {
+    Config config;
+    char linea[100];
+    while (fgets(linea, sizeof(linea), archivo)) {
 		if (linea[0] == '#' || strlen(linea) < 3) continue; // Ignorar comentarios y líneas vacías
 		if (strstr(linea, "LIMITE_RETIRO")) sscanf(linea, "LIMITE_RETIRO=%d", &config.limite_retiro);
 		else if (strstr(linea, "LIMITE_TRANSFERENCIA")) sscanf(linea, "LIMITE_TRANSFERENCIA=%d", &config.limite_transferencia);
@@ -48,14 +52,18 @@ Config leer_configuracion(const char *ruta) {
 	}
 
 	fclose(archivo);
-	return config;}
+	sem_post(sem_banco);
+	return config;
+}
 
 void registrar_operacion_log(const char *ruta_log, Operacion op) {
+	sem_wait(sem_banco);
 	FILE *log = fopen(ruta_log, "a");
 	if (!log) {
-        	perror("Error al abrir archivo de log");
-	        return;
-        }
+        perror("Error al abrir archivo de log");
+		sem_post(sem_banco);
+	    return;
+    }
 
         time_t t = time(NULL);
         struct tm *tm_info = localtime(&t);
@@ -71,19 +79,25 @@ void registrar_operacion_log(const char *ruta_log, Operacion op) {
 	fclose(log);
 }
 
-int main(){
+int main() {
+	//Se crea el semáforo
+	sem_banco = sem_open("/sem_banco", O_CREAT, 0644, 1);
+	if(sem_banco == SEM_FAILED){
+		perror("Error al crear el semáforo");
+		exit(1);
+	}
 	//Se lee la configuración del archivo "config.txt"
 	Config configuracion = leer_configuracion("./config.txt");
-        printf("Archivo de cuentas: %s\n", configuracion.archivo_cuentas);
+	printf("Archivo de cuentas: %s\n", configuracion.archivo_cuentas);
 
-        printf("Número máximo de hilos: %d\n", configuracion.num_hilos);
-
-	pthread_t hilos[configuracion.num_hilos];
+	printf("Número máximo de hilos: %d\n", configuracion.num_hilos);
 
 	//Se abre el archivo de cuentas
+	sem_wait(sem_banco);
 	FILE *archivo = fopen(configuracion.archivo_cuentas, "rb+");
 	if (archivo == NULL){
 		perror("Error al abrir el archivo de cuentas\n");
+		sem_post(sem_banco);
 		exit(1);
 	}
 
@@ -92,16 +106,6 @@ int main(){
 		perror("Error al crear la tubería\n");
 		exit(1);
 	}
-
-	//Se crea el semáforo
-	key_t key = ftok("archivo_clave", 'A');
-	semid = semget(key, 1, IPC_CREAT | 0666);
-	if (semid == -1){
-		perror("Error al crear el semáforo");
-		exit(1);
-	}
-
-	semctl(semid, 0, SETVAL, 1); //Se inicializa el semáforo a 1
 
 	//Se crea un proceso hijo para manejar el menú del usuario
 	pid_t pid = fork();
@@ -119,7 +123,7 @@ int main(){
 	} else {
 	        // Proceso padre
         	close(pipe_padre_hijo[1]); // Cerramos escritura
-    
+/*
 		Operacion op;
         	while (read(pipe_padre_hijo[0], &op, sizeof(op)) > 0) { // Recibimos datos
 	        	printf("\n[PADRE] Operación recibida: %s | Cuenta: %d", op.tipo_operacion, op.cuenta_origen);
@@ -132,7 +136,7 @@ int main(){
 			if (op.monto > configuracion.limite_retiro){
            			printf("Error: Retiro excede el límite permitido (%d)\n", configuracion.limite_retiro);
 		        }
-        	}
+        	}*/
     	}
 
 	//Se cierra la lectura del proceso padre
@@ -141,11 +145,9 @@ int main(){
 	//Esperamos a que el proceso hijo acabe
 	wait(NULL);
 
-	//Se cierra el semáforo
-	if (semctl(semid, 0, IPC_RMID) == -1){
-		perror("Error al eliminar el semáforo");
-		exit(1);
-	}
+	//Se cierra y se elimina el semáforo
+	sem_close(sem_banco);
+	sem_unlink("/sem_banco");
 
 	return 0;
 }
